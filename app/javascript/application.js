0f30122bd88f1f02b8fc1e57945e992bde5f6563
kibirige <kibirigejohn005@gmail.com>
2023-01-29 22:56:17 +0300

    dipatch the action for fetching a greeting from the api

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import ReactDOM from "react-dom";
-
+import React from 'react';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import App from "./components/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchGreeting } from '../redux/greeting'
 
 const Greeting = () => {
+    const {greeting, status} = useSelector(state => state.greeting)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(status === 'idle') {
+            dispatch(fetchGreeting())
+        }
+    }, [status])
+
   return (
     <div>
-        <h1>Hello, World!</h1>
+        <h1>{greeting.message}</h1>
     </div>
   )
 }
